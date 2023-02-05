d92500a7163c0798cec013ed9c1635e68330434f
Ashutosh <theashutosh369@gmail.com>
2023-02-05 11:21:27 +0530

    modified some files

---
+++ PRACTICE/guassing_number.java
@@ -0,0 +1,107 @@
+package CODECHEF.PRACTICE;
+// Ashutosh guassing_numberirajdar
+
+/*
+                                   || कृष्ण सदा सहायते ||
+
+                       `//:-`                                          `````      `
+    `                  -+/:++/-`                                       ```      `
+                    ```/+/::/+++:```` ``                              `                  ````
+                `..-.`./o:o::/+/o-     `...`                       `        `          ````
+             `.-..````-o+/o//yhyoo.      ```..`                         ````          ` `
+    `      `...````` `./++/+sMNdso.```   ``````.`                     ```
+      `   `.`````   .///oo+//sooo+.++:/:`````````.`                  ````             ``````
+       `.````       /+o+/oooosooyoo+////++.````````.`               ```               ``        ```
+      ````          -oo+++++/o+//+o++o/+ooo- ```````..                                       ````
+     ``             //s//-:ooso:/+/yss+shos+` ```````.-                                     ````
+    .`             .++s++//+//y+//+sossooo+++`     ` ``.            `...-.                ``
+   ..          `./+s+oss++.:+/o+////o/:-//::/o`        ``      .:-::/:. /`      ```
+  `-`        `.+o+/ysoy/:+/:/:/:+/:+/--` `:`:.:         `  .-/o:o:-. ``..                 `
+  -`        .++-/ooy++oso/::.--/s+y+-     `/.-`:        .:oyss/.:   ```-                 `
+  -``      -+::/+-/+sssso-:.://+/oo: `:+sys:://s.     ./+:y++s.-`   .`/    `          ``
+  -``    `-+--++/++s+oso////+////++/.:.:/+ss:-+ys`   :+/:-+-.:``-   `:.  `         ``
+ `.``    `+/-/+/++oy:++oo+o/::/:/+:/` :o+shs/../o-  //+:-.--``` :`  ./            ` `
+ `-.`    -o://+-.-:s::o+oo+::/o++:o/``---.````  `.::o.s:--`..`  `- ./`           ``
+  -``   `:+-:////++ss/oo++o/++++sd/-.`        --//oss:/+---.```  :.-             ` ``
+  -.``   :+-/:///:::oy/++os+++osyNo+/.``      `.-osyyo-+/-----://s-             ````
+  `-``   `o/-/+-.-:+oyo/+ooo+.+y+hdds:...`    `:+osoo+o/o+oss+//o++          ````
+   ..``   :+.:///++/:/y+:sooo//+/:/+/:--.`     `-// ``-`++ss+-:-+:-:       ```
+    -.``  .//-://+-.-oso/oo+o+oo/+/-::--.``       .` ` `+/+o/.:-:-/+`     `           ``
+    `-``   .:+`./+/++o-o+:oossyhNmo::::--..````.--/..` :-:oo///:/++:/
+  `   .``   `oo:--:/++:so/syhhmshyyy/::--:--:+so:-.-   o+oo///::-:+os:
+        ``    :oo../ohmmmdddmmmmdhs::----:/+o+o``-.   -s/:.        `.+
+        ```    `ohmNNmhs++++:-/+oss/..` ```.-+.`.     ++:-..`        :`       ``           `
+       `` `-. `odMMdhyyso+:.:+/+ooo+ssosooossyso++-.-/o/:-...``       -      ``            `
+      `     `.oNMMhosssso++///osoo+-:ssso+yho+/+++++o++:----.....`    :
+             -mMMy/oossss//+sooo+-.`+/++s+:/o:++ooooo+///:....`       .-
+          `-/hMMm+oo+o+sossooo//:-..+o+os+://+++ooo+oo+/:::-.````      :                `
+        -shmmMMMhssoyssssyso+:/:-:`::/+syo+soo++o/:/:+/---...-.`       .             ``
+      `+ddmMMMMMmsoossooo+////+/:-.:::-/o+oss/:-/:`-::::``    ``       :             `
+      -+mmNMMMMMNo+//++//////++:/-::/:-+o::+s/../:..-`.:-             -`
+    ``/oNNMMMMMMN+//+++//++//////::/--:os++oo.``/:``+.  .-`         `:`
+   ``-oomMMMMNNMm/:/+/+//+/::///////:--+so+oo`  :/ `::   `      ` `.-`
+   ``.-::ddNNNNMm/-/+:o++/::/::::::/::-:+ysoo-  `:---.`` ``.-::--.-`
+        .:o/ydmmd/-+//o+///+++/++/+/:/:/oyyss++o++os+os://o/+`
+        ...`o:sdm::+:/+++ooo++osyyyy+y///yo+ysoo/oys+so+o+//:o                   ``
+         ` `/.-hy+:/+/++//:/:://syss/so++ys+oyoo+soy/ys:o+///::        ````````````````
+              `:-+-://::::/-///:ooyoo-:-`hyo/o+++++/osy/o/+o::
+         ``    ``-+:/:::::::::::+oyo+..`.sy+/.-/o+--oohy++::+.
+                  o:-////:---:-./oo++.`.`oy+o``.s+:-s+so/:.`-.        ````
+      `           -+-///::-:::-.:+o+/-.``os/o `-os:-ooso:/.``-       ```````````  `````
+     `            `+-::-::-----.:+s+/...`+o/o`` ...-soo+:/``.-      ` ````` `````````````````
+                   -+::::-.-..---+y//-..`:s++/`  `.++sy/::``:.     ` `.-.````````
+                   .o:-:.--.--:--oos/+////o+o+/--://:sy///:-/:     ````...```    `
+     ``````        .+--..-------:/ss/s//+:o+://o++/++oyoo/+/+o.      `````              `
+ ```````````       `+:.---::-.-.-/ys+++o++oo+/oooy+s+yys+/+//-:.     `` `
+   ` ``             +.-.-::-.--`:/os++o-o/ooo++/++++ossy/oo//:`-.  ```` ``````` ` ``````````` `
+                    +....:-.--..-/+y/+:/:::-/oooosoo+sy+s///+...:.      ```````````````````````
+                    -/`--...-....//s/:-.-./``..///+::oy:+//-/`  `:.
+*/
+
+import java.util.*;
+
+public class guassing_number {
+    static Scanner s = new Scanner(System.in);
+
+    static long gcd(long a, long b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+    static void AshutoshKaUniqueAnswer () {
+        long a = s.nextLong();
+        long b = s.nextLong();
+
+        long alice = 0 ;
+
+        if( b%2==0  ) alice+= a*(b/2);
+        else {
+            if( a%2==0 )
+            {
+                alice += ( (a/2) *(b/2) ) + ( (a/2)*(b/2 +1 ) ) ;
+            }
+            else {
+                alice += ( (a/2) *(b/2+1) ) + ( (a-a/2)*(b/2) ) ;
+            }
+        }
+
+        long tot = a*b ;
+
+        long gcd = gcd( alice , tot );
+        System.out.println( alice/gcd + "/" + tot/gcd );
+
+
+    }
+
+    public static void main ( String[] args ) {
+
+        int t = s.nextInt();
+        for (int test = 0; test < t; test++) {
+            AshutoshKaUniqueAnswer();
+        }
+        s.close();
+        //-------------------------------------------
+    }
+}
+
