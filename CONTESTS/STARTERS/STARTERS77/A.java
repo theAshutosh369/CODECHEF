a6b39ac4941d0ece06e202a32a93896ece2f90d2
Ashutosh <theashutosh369@gmail.com>
2023-02-16 08:20:24 +0530

    modified some files

---
+++ CONTESTS/STARTERS/STARTERS77/A.java
@@ -0,0 +1,135 @@
+package CODECHEF.CONTESTS.STARTERS.STARTERS77;
+// Ashutosh Airajdar
+
+/*
+                                   || कृष्ण सदा सहायते ||
+
+                       `//:-`                                          `````      `
+    `                  -+/:++/-`                                       ```      `
+                    ```/+/::/+++:```` ``                              `                  ````
+                `..-.`./o:o::/+/o-     `...`                       `        `          ````
+             `.-..````-o+/o//yhyoo.      ```..`                         ````          ` `
+    `      `...````` `./++/+sMNdso.```   ``````.`                     ```
+      `   `.`````   .///oo+//sooo+.++:/:`````````.`                  ````             ``````
+       `.````       /+o+/oooosooyoo+////++.````````.`               ```               ``        ```
+      ````          -oo+++++/o+//+o++o/+ooo- ```````..                                       ````
+     ``             //s//-:ooso:/+/yss+shos+` ```````.-                                     ````
+    .`             .++s++//+//y+//+sossooo+++`     ` ``.            `...-.                ``
+   ..          `./+s+oss++.:+/o+////o/:-//::/o`        ``      .:-::/:. /`      ```
+  `-`        `.+o+/ysoy/:+/:/:/:+/:+/--` `:`:.:         `  .-/o:o:-. ``..                 `
+  -`        .++-/ooy++oso/::.--/s+y+-     `/.-`:        .:oyss/.:   ```-                 `
+  -``      -+::/+-/+sssso-:.://+/oo: `:+sys:://s.     ./+:y++s.-`   .`/    `          ``
+  -``    `-+--++/++s+oso////+////++/.:.:/+ss:-+ys`   :+/:-+-.:``-   `:.  `         ``
+ `.``    `+/-/+/++oy:++oo+o/::/:/+:/` :o+shs/../o-  //+:-.--``` :`  ./            ` `
+ `-.`    -o://+-.-:s::o+oo+::/o++:o/``---.````  `.::o.s:--`..`  `- ./`           ``
+  -``   `:+-:////++ss/oo++o/++++sd/-.`        --//oss:/+---.```  :.-             ` ``
+  -.``   :+-/:///:::oy/++os+++osyNo+/.``      `.-osyyo-+/-----://s-             ````
+  `-``   `o/-/+-.-:+oyo/+ooo+.+y+hdds:...`    `:+osoo+o/o+oss+//o++          ````
+   ..``   :+.:///++/:/y+:sooo//+/:/+/:--.`     `-// ``-`++ss+-:-+:-:       ```
+    -.``  .//-://+-.-oso/oo+o+oo/+/-::--.``       .` ` `+/+o/.:-:-/+`     `           ``
+    `-``   .:+`./+/++o-o+:oossyhNmo::::--..````.--/..` :-:oo///:/++:/
+  `   .``   `oo:--:/++:so/syhhmshyyy/::--:--:+so:-.-   o+oo///::-:+os:
+        ``    :oo../ohmmmdddmmmmdhs::----:/+o+o``-.   -s/:.        `.+
+        ```    `ohmNNmhs++++:-/+oss/..` ```.-+.`.     ++:-..`        :`       ``           `
+       `` `-. `odMMdhyyso+:.:+/+ooo+ssosooossyso++-.-/o/:-...``       -      ``            `
+      `     `.oNMMhosssso++///osoo+-:ssso+yho+/+++++o++:----.....`    :
+             -mMMy/oossss//+sooo+-.`+/++s+:/o:++ooooo+///:....`       .-
+          `-/hMMm+oo+o+sossooo//:-..+o+os+://+++ooo+oo+/:::-.````      :                `
+        -shmmMMMhssoyssssyso+:/:-:`::/+syo+soo++o/:/:+/---...-.`       .             ``
+      `+ddmMMMMMmsoossooo+////+/:-.:::-/o+oss/:-/:`-::::``    ``       :             `
+      -+mmNMMMMMNo+//++//////++:/-::/:-+o::+s/../:..-`.:-             -`
+    ``/oNNMMMMMMN+//+++//++//////::/--:os++oo.``/:``+.  .-`         `:`
+   ``-oomMMMMNNMm/:/+/+//+/::///////:--+so+oo`  :/ `::   `      ` `.-`
+   ``.-::ddNNNNMm/-/+:o++/::/::::::/::-:+ysoo-  `:---.`` ``.-::--.-`
+        .:o/ydmmd/-+//o+///+++/++/+/:/:/oyyss++o++os+os://o/+`
+        ...`o:sdm::+:/+++ooo++osyyyy+y///yo+ysoo/oys+so+o+//:o                   ``
+         ` `/.-hy+:/+/++//:/:://syss/so++ys+oyoo+soy/ys:o+///::        ````````````````
+              `:-+-://::::/-///:ooyoo-:-`hyo/o+++++/osy/o/+o::
+         ``    ``-+:/:::::::::::+oyo+..`.sy+/.-/o+--oohy++::+.
+                  o:-////:---:-./oo++.`.`oy+o``.s+:-s+so/:.`-.        ````
+      `           -+-///::-:::-.:+o+/-.``os/o `-os:-ooso:/.``-       ```````````  `````
+     `            `+-::-::-----.:+s+/...`+o/o`` ...-soo+:/``.-      ` ````` `````````````````
+                   -+::::-.-..---+y//-..`:s++/`  `.++sy/::``:.     ` `.-.````````
+                   .o:-:.--.--:--oos/+////o+o+/--://:sy///:-/:     ````...```    `
+     ``````        .+--..-------:/ss/s//+:o+://o++/++oyoo/+/+o.      `````              `
+ ```````````       `+:.---::-.-.-/ys+++o++oo+/oooy+s+yys+/+//-:.     `` `
+   ` ``             +.-.-::-.--`:/os++o-o/ooo++/++++ossy/oo//:`-.  ```` ``````` ` ``````````` `
+                    +....:-.--..-/+y/+:/:::-/oooosoo+sy+s///+...:.      ```````````````````````
+                    -/`--...-....//s/:-.-./``..///+::oy:+//-/`  `:.
+*/
+
+import java.util.*;
+
+public class A {
+    static Scanner s = new Scanner(System.in);
+
+    static void AshutoshKaUniqueAnswer () {
+        int n = s.nextInt();
+        int m = s.nextInt();
+        String a = s.next();
+        String b = s.next();
+
+        Map<Character , Integer> mp = new HashMap<>();
+        if( m>n )
+        {
+            String temp = b;
+            b = a;
+            a = temp;
+
+            int k = m ;
+            m = n ;
+            n = k ;
+        }
+
+        for (int i = 0; i < n; i++) {
+            char ch = a.charAt(i);
+            mp.put( ch , mp.getOrDefault( ch , 0 )+1 );
+        }
+        for (int i = 0; i < m; i++) {
+            char ch = b.charAt(i);
+            mp.put( ch , mp.getOrDefault( ch , 0 )-1 );
+        }
+
+        int flag = 1 , c =0 , d = n-m ;
+
+        for( Map.Entry<Character ,Integer> e : mp.entrySet() )
+        {
+            if( e.getValue()<0 ){
+                flag=0;
+                break;
+            }
+            if( e.getValue()%2==0 )
+            {
+                continue;
+            }
+            if( c ==1 )
+            {
+                flag=0;
+            }
+            else c=1;
+        }
+
+
+
+        if( flag==1  )
+        {
+            System.out.println( "YES");
+        }
+        else {
+           System.out.println( "NO");
+        }
+
+
+    }
+
+    public static void main ( String[] args ) {
+
+        int t = s.nextInt();
+        for (int test = 0; test < t; test++) {
+            AshutoshKaUniqueAnswer();
+        }
+        s.close();
+        //-------------------------------------------
+    }
+}
+
---
+++ PRACTICE/chef_and_problem_settings.java
@@ -0,0 +1,108 @@
+package CODECHEF.PRACTICE;
+// Ashutosh chef_and_problem_settingsirajdar
+
+/*
+                                   || कृष्ण सदा सहायते ||
+
+                       `//:-`                                          `````      `
+    `                  -+/:++/-`                                       ```      `
+                    ```/+/::/+++:```` ``                              `                  ````
+                `..-.`./o:o::/+/o-     `...`                       `        `          ````
+             `.-..````-o+/o//yhyoo.      ```..`                         ````          ` `
+    `      `...````` `./++/+sMNdso.```   ``````.`                     ```
+      `   `.`````   .///oo+//sooo+.++:/:`````````.`                  ````             ``````
+       `.````       /+o+/oooosooyoo+////++.````````.`               ```               ``        ```
+      ````          -oo+++++/o+//+o++o/+ooo- ```````..                                       ````
+     ``             //s//-:ooso:/+/yss+shos+` ```````.-                                     ````
+    .`             .++s++//+//y+//+sossooo+++`     ` ``.            `...-.                ``
+   ..          `./+s+oss++.:+/o+////o/:-//::/o`        ``      .:-::/:. /`      ```
+  `-`        `.+o+/ysoy/:+/:/:/:+/:+/--` `:`:.:         `  .-/o:o:-. ``..                 `
+  -`        .++-/ooy++oso/::.--/s+y+-     `/.-`:        .:oyss/.:   ```-                 `
+  -``      -+::/+-/+sssso-:.://+/oo: `:+sys:://s.     ./+:y++s.-`   .`/    `          ``
+  -``    `-+--++/++s+oso////+////++/.:.:/+ss:-+ys`   :+/:-+-.:``-   `:.  `         ``
+ `.``    `+/-/+/++oy:++oo+o/::/:/+:/` :o+shs/../o-  //+:-.--``` :`  ./            ` `
+ `-.`    -o://+-.-:s::o+oo+::/o++:o/``---.````  `.::o.s:--`..`  `- ./`           ``
+  -``   `:+-:////++ss/oo++o/++++sd/-.`        --//oss:/+---.```  :.-             ` ``
+  -.``   :+-/:///:::oy/++os+++osyNo+/.``      `.-osyyo-+/-----://s-             ````
+  `-``   `o/-/+-.-:+oyo/+ooo+.+y+hdds:...`    `:+osoo+o/o+oss+//o++          ````
+   ..``   :+.:///++/:/y+:sooo//+/:/+/:--.`     `-// ``-`++ss+-:-+:-:       ```
+    -.``  .//-://+-.-oso/oo+o+oo/+/-::--.``       .` ` `+/+o/.:-:-/+`     `           ``
+    `-``   .:+`./+/++o-o+:oossyhNmo::::--..````.--/..` :-:oo///:/++:/
+  `   .``   `oo:--:/++:so/syhhmshyyy/::--:--:+so:-.-   o+oo///::-:+os:
+        ``    :oo../ohmmmdddmmmmdhs::----:/+o+o``-.   -s/:.        `.+
+        ```    `ohmNNmhs++++:-/+oss/..` ```.-+.`.     ++:-..`        :`       ``           `
+       `` `-. `odMMdhyyso+:.:+/+ooo+ssosooossyso++-.-/o/:-...``       -      ``            `
+      `     `.oNMMhosssso++///osoo+-:ssso+yho+/+++++o++:----.....`    :
+             -mMMy/oossss//+sooo+-.`+/++s+:/o:++ooooo+///:....`       .-
+          `-/hMMm+oo+o+sossooo//:-..+o+os+://+++ooo+oo+/:::-.````      :                `
+        -shmmMMMhssoyssssyso+:/:-:`::/+syo+soo++o/:/:+/---...-.`       .             ``
+      `+ddmMMMMMmsoossooo+////+/:-.:::-/o+oss/:-/:`-::::``    ``       :             `
+      -+mmNMMMMMNo+//++//////++:/-::/:-+o::+s/../:..-`.:-             -`
+    ``/oNNMMMMMMN+//+++//++//////::/--:os++oo.``/:``+.  .-`         `:`
+   ``-oomMMMMNNMm/:/+/+//+/::///////:--+so+oo`  :/ `::   `      ` `.-`
+   ``.-::ddNNNNMm/-/+:o++/::/::::::/::-:+ysoo-  `:---.`` ``.-::--.-`
+        .:o/ydmmd/-+//o+///+++/++/+/:/:/oyyss++o++os+os://o/+`
+        ...`o:sdm::+:/+++ooo++osyyyy+y///yo+ysoo/oys+so+o+//:o                   ``
+         ` `/.-hy+:/+/++//:/:://syss/so++ys+oyoo+soy/ys:o+///::        ````````````````
+              `:-+-://::::/-///:ooyoo-:-`hyo/o+++++/osy/o/+o::
+         ``    ``-+:/:::::::::::+oyo+..`.sy+/.-/o+--oohy++::+.
+                  o:-////:---:-./oo++.`.`oy+o``.s+:-s+so/:.`-.        ````
+      `           -+-///::-:::-.:+o+/-.``os/o `-os:-ooso:/.``-       ```````````  `````
+     `            `+-::-::-----.:+s+/...`+o/o`` ...-soo+:/``.-      ` ````` `````````````````
+                   -+::::-.-..---+y//-..`:s++/`  `.++sy/::``:.     ` `.-.````````
+                   .o:-:.--.--:--oos/+////o+o+/--://:sy///:-/:     ````...```    `
+     ``````        .+--..-------:/ss/s//+:o+://o++/++oyoo/+/+o.      `````              `
+ ```````````       `+:.---::-.-.-/ys+++o++oo+/oooy+s+yys+/+//-:.     `` `
+   ` ``             +.-.-::-.--`:/os++o-o/ooo++/++++ossy/oo//:`-.  ```` ``````` ` ``````````` `
+                    +....:-.--..-/+y/+:/:::-/oooosoo+sy+s///+...:.      ```````````````````````
+                    -/`--...-....//s/:-.-./``..///+::oy:+//-/`  `:.
+*/
+
+import java.util.*;
+
+public class chef_and_problem_settings {
+    static Scanner s = new Scanner(System.in);
+
+    static void AshutoshKaUniqueAnswer () {
+        int n = s.nextInt();
+        int m = s.nextInt();
+
+        int invalid = 0 , weak =0 ;
+        for (int i = 0; i < n; i++) {
+            String str = s.next();
+            String test = s.next();
+
+            if( str.equals("correct") ) {
+                for (int j = 0; j < m; j++) {
+                    if( test.charAt(j)=='0') {
+                        invalid=1;
+                        break;
+                    }
+                }
+            }else {
+                int c = 0 ;
+                for (int j = 0; j < m; j++) {
+                    if( test.charAt(j)=='1') c++;
+                }
+                if( c==m && invalid==0 ) {
+                    weak=1;
+                }
+            }
+        }
+        if( invalid==1 ) System.out.println( "INVALID");
+        else if( weak==1 ) System.out.println("WEAK");
+        else System.out.println("FINE");
+
+    }
+
+    public static void main ( String[] args ) {
+
+        int t = s.nextInt();
+        for (int test = 0; test < t; test++) {
+            AshutoshKaUniqueAnswer();
+        }
+        s.close();
+        //-------------------------------------------
+    }
+}
+
