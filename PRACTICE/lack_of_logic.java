a9d4a155426b79d5f87a3ec0a425908ffa347194
Ashutosh <theashutosh369@gmail.com>
2023-03-08 16:51:44 +0530

    modified some files

---
+++ PRACTICE/lack_of_logic.java
@@ -0,0 +1,108 @@
+package CODECHEF.PRACTICE;
+// Ashutosh lack_of_logicirajdar
+
+/*
+                                   || कृष्ण सदा सहायते ||
+
+                       `//:-`                                          `````      `
+    `                  -+/:++/-`                                       ```      `
+                    ```/+/::/+++:```` ``                              `                  ````
+                `..-.`./o:o::/+/o-     `...`                       `        `          ````
+             `.-..````-o+/o//yhyoo.      ```..`                         ````          ` `
+    `      `...````` `./++/+sMNdso.```   ``````.`                     ```
+      `   `.`````   .///oo+//sooo+.++:/:`````````.`                  ````             ``````
+       `.````       /+o+/oooosooyoo+////++.````````.`               ```               ``        ```
+      ````          -oo+++++/o+//+o++o/+ooo- ```````..                                       ````
+     ``             //s//-:ooso:/+/yss+shos+` ```````.-                                     ````
+    .`             .++s++//+//y+//+sossooo+++`     ` ``.            `...-.                ``
+   ..          `./+s+oss++.:+/o+////o/:-//::/o`        ``      .:-::/:. /`      ```
+  `-`        `.+o+/ysoy/:+/:/:/:+/:+/--` `:`:.:         `  .-/o:o:-. ``..                 `
+  -`        .++-/ooy++oso/::.--/s+y+-     `/.-`:        .:oyss/.:   ```-                 `
+  -``      -+::/+-/+sssso-:.://+/oo: `:+sys:://s.     ./+:y++s.-`   .`/    `          ``
+  -``    `-+--++/++s+oso////+////++/.:.:/+ss:-+ys`   :+/:-+-.:``-   `:.  `         ``
+ `.``    `+/-/+/++oy:++oo+o/::/:/+:/` :o+shs/../o-  //+:-.--``` :`  ./            ` `
+ `-.`    -o://+-.-:s::o+oo+::/o++:o/``---.````  `.::o.s:--`..`  `- ./`           ``
+  -``   `:+-:////++ss/oo++o/++++sd/-.`        --//oss:/+---.```  :.-             ` ``
+  -.``   :+-/:///:::oy/++os+++osyNo+/.``      `.-osyyo-+/-----://s-             ````
+  `-``   `o/-/+-.-:+oyo/+ooo+.+y+hdds:...`    `:+osoo+o/o+oss+//o++          ````
+   ..``   :+.:///++/:/y+:sooo//+/:/+/:--.`     `-// ``-`++ss+-:-+:-:       ```
+    -.``  .//-://+-.-oso/oo+o+oo/+/-::--.``       .` ` `+/+o/.:-:-/+`     `           ``
+    `-``   .:+`./+/++o-o+:oossyhNmo::::--..````.--/..` :-:oo///:/++:/
+  `   .``   `oo:--:/++:so/syhhmshyyy/::--:--:+so:-.-   o+oo///::-:+os:
+        ``    :oo../ohmmmdddmmmmdhs::----:/+o+o``-.   -s/:.        `.+
+        ```    `ohmNNmhs++++:-/+oss/..` ```.-+.`.     ++:-..`        :`       ``           `
+       `` `-. `odMMdhyyso+:.:+/+ooo+ssosooossyso++-.-/o/:-...``       -      ``            `
+      `     `.oNMMhosssso++///osoo+-:ssso+yho+/+++++o++:----.....`    :
+             -mMMy/oossss//+sooo+-.`+/++s+:/o:++ooooo+///:....`       .-
+          `-/hMMm+oo+o+sossooo//:-..+o+os+://+++ooo+oo+/:::-.````      :                `
+        -shmmMMMhssoyssssyso+:/:-:`::/+syo+soo++o/:/:+/---...-.`       .             ``
+      `+ddmMMMMMmsoossooo+////+/:-.:::-/o+oss/:-/:`-::::``    ``       :             `
+      -+mmNMMMMMNo+//++//////++:/-::/:-+o::+s/../:..-`.:-             -`
+    ``/oNNMMMMMMN+//+++//++//////::/--:os++oo.``/:``+.  .-`         `:`
+   ``-oomMMMMNNMm/:/+/+//+/::///////:--+so+oo`  :/ `::   `      ` `.-`
+   ``.-::ddNNNNMm/-/+:o++/::/::::::/::-:+ysoo-  `:---.`` ``.-::--.-`
+        .:o/ydmmd/-+//o+///+++/++/+/:/:/oyyss++o++os+os://o/+`
+        ...`o:sdm::+:/+++ooo++osyyyy+y///yo+ysoo/oys+so+o+//:o                   ``
+         ` `/.-hy+:/+/++//:/:://syss/so++ys+oyoo+soy/ys:o+///::        ````````````````
+              `:-+-://::::/-///:ooyoo-:-`hyo/o+++++/osy/o/+o::
+         ``    ``-+:/:::::::::::+oyo+..`.sy+/.-/o+--oohy++::+.
+                  o:-////:---:-./oo++.`.`oy+o``.s+:-s+so/:.`-.        ````
+      `           -+-///::-:::-.:+o+/-.``os/o `-os:-ooso:/.``-       ```````````  `````
+     `            `+-::-::-----.:+s+/...`+o/o`` ...-soo+:/``.-      ` ````` `````````````````
+                   -+::::-.-..---+y//-..`:s++/`  `.++sy/::``:.     ` `.-.````````
+                   .o:-:.--.--:--oos/+////o+o+/--://:sy///:-/:     ````...```    `
+     ``````        .+--..-------:/ss/s//+:o+://o++/++oyoo/+/+o.      `````              `
+ ```````````       `+:.---::-.-.-/ys+++o++oo+/oooy+s+yys+/+//-:.     `` `
+   ` ``             +.-.-::-.--`:/os++o-o/ooo++/++++ossy/oo//:`-.  ```` ``````` ` ``````````` `
+                    +....:-.--..-/+y/+:/:::-/oooosoo+sy+s///+...:.      ```````````````````````
+                    -/`--...-....//s/:-.-./``..///+::oy:+//-/`  `:.
+*/
+
+import java.util.*;
+
+public class lack_of_logic {
+
+    static Scanner s = new Scanner(System.in);
+
+    static void AshutoshKaUniqueAnswer ()
+    {
+        String str = s.nextLine();
+        int ch[] = new int[26];
+        int n = str.length();
+        for (int i = 0; i < n; i++)
+        {
+            char c = str.charAt(i);
+            if( (c>=65 && c<=90) || (c>=97 && c<=122) )
+            {
+                if (  c <= 90 ) {
+                    c = (char) (c + 32);
+                }
+                ch[c - 97] = 1;
+            }
+        }
+
+        String ans = "";
+        for (int i = 0; i < 26; i++) {
+            if( ch[i]==0 ) {
+                ans+= Character.toString( (char)(i+97));
+                break;
+            }
+        }
+        if( ans.equals("") ) System.out.println("~");
+        else System.out.println( ans );
+    }
+
+    public static void main ( String[] args ) {
+
+
+        int t = Integer.parseInt( s.nextLine() );
+        for (int test = 0; test < t; test++) {
+            AshutoshKaUniqueAnswer();
+        }
+        s.close();
+        //-------------------------------------------
+
+
+    }
+}
+
